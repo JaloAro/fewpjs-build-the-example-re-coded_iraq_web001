17edd35acf631ef92a78ce72096c79cba8d37f01
jalal Arif <jalalharfjalal@gmail.com>
2020-06-06 08:13:04 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -1,38 +1,3 @@
-// Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
-
-// Your JavaScript code goes here!
-
-
-
-
-//------------------------------------------------------------------------------
-// Ignore after this point. Used only for demo purposes
-//------------------------------------------------------------------------------
-
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
-      if (isRandomFailure) {
-        reject("Random server error. Try again.");
-      } else {
-        resolve("Pretend remote server notified of action!");
-      }
-    }, 300);
-  });
-}
-
-
-
-
-
-
-
-
-
-
 
 
 // Defining text characters for the empty and full hearts for you to use later.
